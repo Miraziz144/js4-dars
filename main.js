6811525b0943ca8367ab2159047cdfbfdd9f1edb
Miraziz144 <nurullayevmiraziz4@gmail.com>
2024-06-08 16:08:29 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+// let yosh = prompt ('yoshingizni kiriting')
+
+
+// if ( yosh > 18){
+// alert('saytga kush kelibsiz')
+// }else{
+//     alert('parol xato')
+// }
+
+
+
+
+
+let randomson = Math.random() *100
+
+alert('random chiqqan son'  )
+
+alert(randomson = Math.floor(randomson));
+
+
+console.log(Math.floor(randomson));
